41dd67d1dde3ffb906310e308cab491b84a54c08
codevanss <parthsrivastav7077@gmail.com>
2025-02-08 22:47:49 +0530

    Python

--- self/pyqt_intro.py
+++ self/pyqt_intro.py
@@ -1,13 +1,38 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow , QLabel
 from PyQt5.QtGui import QIcon
+# from PyQt5.QtGui import QFont
+# from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My first GUI")
         self.setGeometry(720,300,500,500)
-        self.setWindowIcon(QIcon('1000077805 (1).jpg')) # image not loading
+        self.setWindowIcon(QIcon("self/download.jpg")) # image not loading
+        # label = QLabel("Hello" , self)
+        # label.setFont(QFont("Arial" , 20))
+        # label.setGeometry(0,0,500,100)
+        # label.setStyleSheet("color : red;"
+        #                     "background-color : #6fdcf7;"
+        #                     "font-weight : bold;"
+        #                     "font-style: italic;"
+        #                     "text-decoration: underline;")
+        # label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter) #(Qt.AlignCenter) is also done the same work
+
+        label = QLabel(self)
+        label.setGeometry(0,0,500,400)
+        pixmap = QPixmap("self/pic.jpg") # this picture is also not loading
+        label.setPixmap(pixmap)
+        label.setScaledContents(True)
+        label.setGeometry((self.width() - label.width())//2,
+                          (self.height()-label.height())//2,
+                          label.width(),
+                          label.height())
+
+
+
 
 def main():
    app = QApplication(sys.argv)
---
+++ self/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+vbox = QVBoxLayout()
+        vbox.addWidget(label1)
+        vbox.addWidget(label2)
+        vbox.addWidget(label3)
+        vbox.addWidget(label4)
+        vbox.addWidget(label5)
+        
+        central_widget.setLayout(vbox) 
