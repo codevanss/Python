27ee5648ceb81b4db6d8ecb19a3d793271593195
codevanss <parthsrivastav7077@gmail.com>
2025-05-01 21:31:44 +0530

    Python

---
+++ self/chatbot.py
@@ -0,0 +1,23 @@
+import google.generativeai as genai
+
+API_KEY = "AIzaSyDNitJL_IdFcQTyPuXzTEiR-EfdKzefYOc"
+
+genai.configure(api_key = API_KEY)
+
+model = genai.GenerativeModel("gemini-2.0-flash")
+
+chat =  model.start_chat()
+
+print ("Chat with gemini! Type 'exit' to quit.")
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == 'exit':
+        print("Exiting chat. Goodbye!")
+        break
+
+
+    try:
+        response = chat.send_message(user_input)
+        print("Gemini:", response.text)
+    except Exception as e:
+        print("Error:", e)
