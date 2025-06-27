c70b34a427962cc57ab1ec54b0b08a5e6b85c066
codevanss <parthsrivastav7077@gmail.com>
2025-06-27 21:43:24 +0530

    Python

---
+++ pascaltriange.py
@@ -0,0 +1,20 @@
+def factorial(n):
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+    return f
+
+def binomial(n, r):
+    return factorial(n) // (factorial(r) * factorial(n - r))
+
+# Input number of rows
+rows = int(input("Enter number of rows: "))
+
+for i in range(rows):
+    # Print leading spaces
+    print("  " * (rows - i - 1), end="")
+    
+    # Print binomial coefficients
+    for j in range(i + 1):
+        print(f"{binomial(i, j):4}", end="")
+    print()
