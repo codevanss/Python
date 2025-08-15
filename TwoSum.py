48efbb5449f7f95471035b946f4166e66fb5770e
codevanss <parthsrivastav7077@gmail.com>
2025-08-15 21:42:14 +0530

    Python

---
+++ TwoSum.py
@@ -0,0 +1,7 @@
+# Two Sum without using + operator
+def add_nums(num1, num2):
+    while num2 != 0:
+        data = num1 & num2
+        num1 = num1 ^ num2
+        num2 = data << 1
+    return num1
