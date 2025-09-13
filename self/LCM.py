7db149df8c12f1c2b96fbfbd73a896fb0e33534e
codevanss <parthsrivastav7077@gmail.com>
2025-09-13 21:28:11 +0530

    Python

---
+++ self/LCM.py
@@ -0,0 +1,12 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return abs(a * b) // gcd(a, b)
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+print(f"The LCM of {num1} and {num2} is {lcm(num1, num2)}")
