6aff85be336253546589fbc24e6f489a66134d67
codevanss <parthsrivastav7077@gmail.com>
2025-10-14 13:28:24 +0530

    Python

---
+++ MediumHard100.py
@@ -0,0 +1,12 @@
+# Q1 Read a text file and count occurrences of each word using regex and Counter.
+import re 
+from collections import Counter 
+def word_count(filepath): 
+    with open(filepath, 'r',encoding='utf-8') as f: 
+        text = f.read().lower() 
+        words = re.findall(r'\b\w+\b', text)
+    return Counter(words) 
+if __name__ == '__main__': 
+    with open('sample.txt','w') as f:
+    f.write('Hello hello world test world') 
+    print(word_count('sample.txt'))
